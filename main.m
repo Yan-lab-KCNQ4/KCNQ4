2a087631dcebe1c3f0ea49760634fc966c92db51
xhyan18 <83393063+xhyan18@users.noreply.github.com>
2021-04-29 15:39:18 +0800

    Calculate the half activated voltage and K value

--- main.m
+++ main.m
@@ -1,5 +1,5 @@
 function main
-SamplePath = 'D:\A研究生\A实验室\KCNQ4\所有数据\C';
+SamplePath = 'D:\A研究生\A实验室\KCNQ4\所有数据\S6  done';
 cd(SamplePath);
 files = dir(SamplePath);
 length = size(files,1);
@@ -7,24 +7,19 @@ names = files(3:length);
 NAMES = {names.name};
 LENGTH = size(NAMES,2);
 row1 = 1;
-col1 = 1;
 row2 = 1;
-for ii = 376 : 378
+for ii = 1 : LENGTH
      AA = NAMES{ii};
      FULLNAME = [SamplePath,'\',AA];
-     cd('D:\A研究生\A实验室\KCNQ4\计算\electric current');
-     [out1,out2,out3] = test(FULLNAME);
+     cd('D:\A研究生\A实验室\KCNQ4\计算\V12_SINGLE');
+     [out1, out2] = Vstest(FULLNAME);
      ROW = size(out2,1);
      COL = size(out2,2);
-     COL2 = size(out1,2);
-     ROW2 = size(out3,1);
+     ROW2 = size(out1,1);
      DD(row1:row1+ROW-1,1:COL) = out2;
-
-     NN(1:8,col1:col1+COL2-1) = out1(1:8,1:COL2);
-     NAME(row2:row2+ROW2-1,1) = out3(1:ROW2);
-     col1 = col1 + COL2;
+     NN(row2:row2+ROW2-1,1) = out1(1:ROW2);
+     row2 = row2 + ROW + 1;
      row1 = row1 + ROW + 1;
-     row2 = row2 + ROW2 + 8;
      clear AA;
      clear FULLNAME;
      clear out1;
