d233e2e7c078eab0989e6772a90ae8060a7c33c6
xhyan18 <83393063+xhyan18@users.noreply.github.com>
2022-03-29 11:15:10 +0800

    Update test.m

--- current density/test.m
+++ current density/test.m
@@ -4,7 +4,8 @@ function [I_C_Data_final_image,RESULT,R] = test(FULLNAME)
 
 Source_Pathname=[FULLNAME,'\*.abf']
 Allfilename=dir(Source_Pathname);
-DST_Pathname='D:\A研究生\A实验室\KCNQ4\计算\electric current';
+% WORK PATH
+DST_Pathname='D:\CALCULATE\electric current';
 filenamelength=length(Allfilename);
 for i=1:filenamelength
     copyfile([FULLNAME,'\',Allfilename(i).name],DST_Pathname);
