9dc5bc15aceb3f5ff11829f25f1dd1df32712c57
xhyan18 <83393063+xhyan18@users.noreply.github.com>
2022-03-29 10:48:45 +0800

    Update origin_cotest.m

--- co-current density/origin_cotest.m
+++ co-current density/origin_cotest.m
@@ -1,10 +1,11 @@
 clear all
 clc
-prompt = '请复制粘贴文件路径 ';
+prompt = 'Please copy and paste the file path ';
 str = input(prompt,'s');
 Source_Pathname=[str,'\*.abf']
 Allfilename=dir(Source_Pathname);
-DST_Pathname='D:\A研究生\A实验室\KCNQ4\计算\co elec current';
+% WORK PATH
+DST_Pathname='D:\CALCULATE\co elec current';
 filenamelength=length(Allfilename);
 for i=1:filenamelength
     copyfile([str,'\',Allfilename(i).name],DST_Pathname);
